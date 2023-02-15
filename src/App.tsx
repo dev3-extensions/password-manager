874e0e15b17c8dff4c7d2d6fca5010f43bd8f957
Aryan Prince <aryanprincesanty@icloud.com>
2023-02-15 20:40:05 +0000

    feat: Can copy generated passwords now * Copies generated password to clipboard * Transition added to indicate copy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Copy, RefreshCw, Save } from 'lucide-react'
+import { Copy, RefreshCw, Save, Check } from 'lucide-react'
 import { useState } from 'react'
 import { PasswordGenerator } from './passwordGenerator'
 
@@ -7,6 +7,7 @@ function App() {
    * State to store the password generated
    */
   const [password, setPassword] = useState('')
+  const [copyStatus, setCopyStatus] = useState(false)
 
   /**
    * Password handler to generate the password
@@ -14,6 +15,16 @@ function App() {
   const handlerGenPassword = () => {
     let generator = new PasswordGenerator()
     setPassword(generator.generatePassword())
+  }
+
+  const copyPassword = () => {
+    navigator.clipboard.writeText(password)
+
+    setCopyStatus(true)
+
+    setTimeout(() => {
+      setCopyStatus(false)
+    }, 1000)
   }
 
   return (
@@ -30,8 +41,11 @@ function App() {
               <RefreshCw size={18} />
               Generate
             </button>
-            <button className="ml-auto rounded-lg border border-[#404040] bg-[#313131] px-3 py-2 font-medium shadow-lg hover:bg-[#254262]">
-              <Copy size={18} />
+            <button
+              className="ml-auto rounded-lg border border-[#404040] bg-[#313131] px-3 py-2 shadow-lg hover:bg-[#254262]"
+              onClick={copyPassword}
+            >
+              {copyStatus ? <Check size={18} /> : <Copy size={18} />}
             </button>
             <button className="rounded-lg border border-[#404040] bg-[#313131] px-3 py-2 font-medium shadow-lg hover:bg-[#254262]">
               <Save size={18} />
