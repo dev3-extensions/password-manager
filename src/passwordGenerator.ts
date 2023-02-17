a6426ac474999a3660a08aea87a1ffa3abc26cdd
A. La Fauci <87176210+Bosurgi@users.noreply.github.com>
2023-02-17 19:03:38 +0000

    feat: Cryptography secure random password generator with options (#6)
    * Feat: Set up Symbols and Numbers algorithm
    
    * Feat: Implemented method to check complexity elements.
    
    * Feat: Implemented forcing symbols and number to TEST.
    
    * Bug: Doesn't generate password
    
    * Feat: Added function to generate random value.
    
    * Feat: Implemented Shuffle, various Random methods.
    
    * Bug: Implemented algorithm random password.
    
    * Freezes although it produces the password
    
    * Problems with setting the length
    
    * Fix: Bug with signed value in algorithm.
    * TODO: Reset flags for each iteration to ensure at least one symbol.
    
    * Fix: Generates symbol or number if selected.
    Now it generates symbols and numbers at least once if selected.

--- src/passwordGenerator.ts
+++ src/passwordGenerator.ts
@@ -1,16 +1,22 @@
 import { PASSWORD_LENGTH } from './constants/passwordLength'
 
 /**
+ * String of data containing string for all password options
+ */
+const PASSWORD_OPTIONS = {
+  NUMBERS: '0123456789',
+  SYMBOLS: '!@#$%^&*()',
+  CHARACTERS_LOWER: 'abcdefghijklmnopqrstuvwxyz',
+  CHARACTERS_UPPER: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+}
+
+let passwordLength: number
+
+/**
  * Class representing a password generator.
  * It generates a random password based on the desired length
  */
 class PasswordGenerator {
-  // Readonly constant with all characters numbers and symbols
-  readonly CHARS = '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  readonly NUMBERS = '0123456789'
-  readonly SYMBOLS = '!@#$%^&*()'
-  readonly CHARACTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
   /**
    *  Function used to generate random password using Crypto Library
    * @returns a randomly generated password.
@@ -20,43 +26,111 @@ class PasswordGenerator {
    * @returns the password generated
    */
   generatePassword(length: number, numbers: boolean, symbols: boolean): string {
+    // Flags to determine if symbols or numbers have been used
+    let isSymbol: boolean = false
+    let isNumber: boolean = false
+    // Initial empty password
     let password: string = ''
-    let chars = this.CHARACTERS
-    if (numbers) {
-      chars += this.NUMBERS
-    }
-    if (symbols) {
-      chars += this.SYMBOLS
-    }
+    let availableChars = PASSWORD_OPTIONS.CHARACTERS_LOWER + PASSWORD_OPTIONS.CHARACTERS_UPPER
 
-    // TODO: refactor into different functions
+    switch (length) {
+      case 0:
+        passwordLength = PASSWORD_LENGTH.SMALL
+        break
 
-    const array: Uint32Array = new Uint32Array(chars.length)
-    let randomValues: Uint32Array = crypto.getRandomValues(array)
+      case 1:
+        passwordLength = PASSWORD_LENGTH.MEDIUM
+        break
 
-    let passwordLength: number
-    if (length == 0) {
-      passwordLength = PASSWORD_LENGTH.SMALL
-    } else if (length == 1) {
-      passwordLength = PASSWORD_LENGTH.MEDIUM
-    } else {
-      passwordLength = PASSWORD_LENGTH.LONG
+      case 2:
+        passwordLength = PASSWORD_LENGTH.LONG
+        break
     }
 
-    // TODO: Bug - Forcing symbol or numbers when flag is on
-    for (let i = 0; i < passwordLength; i++) {
-      let random: number = randomValues[i] % chars.length
-      password += chars.substring(random, random + 1)
+    // The characters pool
+    const charPool: string[] = []
+
+    if (numbers && symbols) {
+      availableChars += PASSWORD_OPTIONS.NUMBERS + PASSWORD_OPTIONS.SYMBOLS
+    } else if (numbers) {
+      availableChars += PASSWORD_OPTIONS.NUMBERS
+    } else if (symbols) {
+      availableChars += PASSWORD_OPTIONS.SYMBOLS
     }
+
+    // Adding numbers and symbols
+    while (charPool.length < passwordLength) {
+      // Checking if number has been added
+      if (numbers && !isNumber) {
+        charPool.push(this.genRandomChar(PASSWORD_OPTIONS.NUMBERS))
+        isNumber = true
+      }
+
+      // Checking if symbol as been added
+      if (symbols && !isSymbol) {
+        charPool.push(this.genRandomChar(PASSWORD_OPTIONS.SYMBOLS))
+        isSymbol = true
+      } else {
+        charPool.push(this.genRandomChar(availableChars))
+      }
+    } // End of While
+
+    // Shuffling the list and returning
+    password = this.shuffleList(charPool).join('')
     return password
+  } // End of Method
+
+  /**
+   * It shuffles characters among random index
+   * @param charArray the Array of string to shuffle
+   * @returns a shuffled array
+   */
+  shuffleList(charArray: string[]) {
+    for (let i = charArray.length - 1; i > 0; i--) {
+      // Generating random index to swap
+      const swapIndex = this.randomNumber(charArray.length)
+      // The character to swap
+      const charToSwap = charArray[i]
+      charArray[i] = charArray[swapIndex]
+      charArray[swapIndex] = charToSwap
+    }
+    return charArray
+  }
+  /**
+   * It gets a truly-random character from a given string
+   * @param string the string we would like to take a character from
+   * @returns a random string representing a random character
+   */
+  genRandomChar(string: string): string {
+    return string[this.randomNumber(string.length)]
   }
 
   /**
-   * It checks the complexity the user wants for the password.
+   * Method which generates a random integer between 0 and Max
+   * Using Fisher-Yates algorithm
+   * @param maxValue the maximum value randomly generated
+   * @returns a random integer between 0 and maxValue
    */
-  checkComplexity(): boolean {
-    // TODO: Implementing this feature
-    return false
+  randomNumber(maxValue: number) {
+    const noBytes = Math.ceil(Math.log2(maxValue / 8))
+    if (!noBytes) {
+      return 0
+    }
+
+    const maxNum = Math.pow(256, noBytes)
+    const bufferArray = new Uint8Array(noBytes)
+
+    while (true) {
+      crypto.getRandomValues(bufferArray)
+      let value = 0
+      for (let i = 0; i < noBytes; i++) {
+        value = (value << 8) + bufferArray[i]
+      }
+      if (value < maxNum - (maxNum % maxValue)) {
+        // Absolute value to avoid signed bits
+        return Math.abs(value % maxValue)
+      }
+    }
   }
 
   /**
