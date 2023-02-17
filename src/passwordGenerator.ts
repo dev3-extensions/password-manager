bccd568872b90c58ec8e5a58c8c33f47da8d2929
A. La Fauci <87176210+Bosurgi@users.noreply.github.com>
2023-02-17 11:53:02 +0000

    Feat: Implemented forcing symbols and number to TEST.

--- src/passwordGenerator.ts
+++ src/passwordGenerator.ts
@@ -25,36 +25,47 @@ class PasswordGenerator {
     generatePassword(length: number, numbers: boolean, symbols: boolean): string {
         let password: string = "";
 
-        /*
-    let chars = this.CHARS
-    if (numbers) {
-      chars += this.NUMBERS
-    }
-    if (symbols) {
-      chars += this.SYMBOLS
-    }
+        // TODO: Change this
+        let chars = this.CHARACTERS_LOWER + this.CHARACTERS_UPPER;
+        if (numbers) {
+            chars += this.NUMBERS;
+        }
+        if (symbols) {
+            chars += this.SYMBOLS;
+        }
 
-    // TODO: refactor into different functions
+        // TODO: refactor into different functions
 
-    const array: Uint32Array = new Uint32Array(chars.length)
-    let randomValues: Uint32Array = crypto.getRandomValues(array)
+        const array: Uint32Array = new Uint32Array(chars.length);
+        let randomValues: Uint32Array = crypto.getRandomValues(array);
 
-    let passwordLength: number
-    if (length == 0) {
-      passwordLength = PASSWORD_LENGTH.SMALL
-    } else if (length == 1) {
-      passwordLength = PASSWORD_LENGTH.MEDIUM
-    } else {
-      passwordLength = PASSWORD_LENGTH.LONG
-    }
+        let passwordLength: number;
+        if (length == 0) {
+            passwordLength = PASSWORD_LENGTH.SMALL;
+        } else if (length == 1) {
+            passwordLength = PASSWORD_LENGTH.MEDIUM;
+        } else {
+            passwordLength = PASSWORD_LENGTH.LONG;
+        }
 
-    // TODO: Bug - Forcing symbol or numbers when flag is on
-    for (let i = 0; i < passwordLength; i++) {
-      let random: number = randomValues[i] % chars.length
-      password += chars.substring(random, random + 1)
-    }
-    return password
-    */
+        // TODO: Bug - Forcing symbol or numbers when flag is on
+        for (let i = 0; i < passwordLength; i++) {
+            let random: number = randomValues[i] % chars.length;
+            password += chars.substring(random, random + 1);
+        }
+
+        if (numbers) {
+            if (!this.checkComplexityElement(password, this.NUMBERS)) {
+                // TODO: change a random character to a number
+                password.replace(Math.random() * (password.length - 1), this.NUMBERS.charAt(Math.random() * (this.NUMBERS.length - 1)));
+            }
+        } else if (symbols) {
+            if (!this.checkComplexityElement(password, this.SYMBOLS)) {
+                // TODO: change a random character to a number
+                password.replace(Math.random() * (password.length - 1), this.SYMBOLS.charAt(Math.random() * (this.SYMBOLS.length - 1)));
+            }
+        }
+        return password;
     }
 
     /**
