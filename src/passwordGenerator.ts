2109d6f2d35b56505970f05a837dacdfdbd4914d
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2023-02-17 14:25:28 +0000

    Feat: Implemented Shuffle, various Random methods.

--- src/passwordGenerator.ts
+++ src/passwordGenerator.ts
@@ -1,8 +1,15 @@
 import { PASSWORD_LENGTH } from './constants/passwordLength'
-const NUMBERS = '0123456789'
-const SYMBOLS = '!@#$%^&*()'
-const CHARACTERS_LOWER = 'abcdefghijklmnopqrstuvwxyz'
-const CHARACTERS_UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+/**
+ * String of data containing string for all password options
+ */
+const PASSWORD_OPTIONS = {
+  NUMBERS: '0123456789',
+  SYMBOLS: '!@#$%^&*()',
+  CHARACTERS_LOWER: 'abcdefghijklmnopqrstuvwxyz',
+  CHARACTERS_UPPER: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+}
+
 /**
  * Class representing a password generator.
  * It generates a random password based on the desired length
@@ -19,49 +26,52 @@ class PasswordGenerator {
    * @returns the password generated
    */
   generatePassword(length: number, numbers: boolean, symbols: boolean): string {
+    // Initial empty password
     let password: string = ''
 
-    // TODO: Change this
-    let chars = this.CHARACTERS_LOWER + this.CHARACTERS_UPPER
-    if (numbers) {
-      chars += this.NUMBERS
-    }
-    if (symbols) {
-      chars += this.SYMBOLS
-    }
+    // The characters pool
+    const charPool = []
 
-    // TODO: refactor into different functions
+    if (numbers && symbols) {
+      let availableChars =
+        PASSWORD_OPTIONS.NUMBERS +
+        PASSWORD_OPTIONS.SYMBOLS +
+        PASSWORD_OPTIONS.CHARACTERS_LOWER +
+        PASSWORD_OPTIONS.CHARACTERS_UPPER
 
-    const array: Uint32Array = new Uint32Array(chars.length)
-    let randomValues: Uint32Array = crypto.getRandomValues(array)
-
-    let passwordLength: number
-    if (length == 0) {
-      passwordLength = PASSWORD_LENGTH.SMALL
-    } else if (length == 1) {
-      passwordLength = PASSWORD_LENGTH.MEDIUM
-    } else {
-      passwordLength = PASSWORD_LENGTH.LONG
+      // Adding numbers and symbols
+      //password += PASSWORD_OPTIONS.NUMBERS + PASSWORD_OPTIONS.SYMBOLS
+      charPool.push(this.genRandomChar(availableChars))
     }
-
-    // TODO: Bug - Forcing symbol or numbers when flag is on
-    for (let i = 0; i < passwordLength; i++) {
-      let random: number = randomValues[i] % chars.length
-      password += chars.substring(random, random + 1)
-    }
-    return password
   }
 
   /**
-   * It checks the complexity the user wants for the password.
+   * It shuffles characters among random index
+   * @param charArray the Array of string to shuffle
+   * @returns a shuffled array
    */
-  checkComplexityElement(password: string, array): boolean {
-    let passwordArray = password.split('')
-    return passwordArray.some((r) => array(r))
+  shuffleList(charArray: string[]) {
+    for (let i = charArray.length - 1; i > 0; i--) {
+      // Generating random index to swap
+      const swapIndex = this.randomNumber(charArray.length)
+      const charToSwap = charArray[i]
+      charArray[i] = charArray[swapIndex]
+      charArray[swapIndex] = charToSwap
+    }
+    return charArray
+  }
+  /**
+   * It gets a truly-random character from a given string
+   * @param string the string we would like to take a character from
+   * @returns a random string representing a random character
+   */
+  genRandomChar(string: string): string {
+    return string[this.randomNumber(string.length)]
   }
 
   /**
    * Method which generates a random integer between 0 and Max
+   * Using Fisher-Yates algorithm
    * @param maxValue the maximum value randomly generated
    * @returns a random integer between 0 and maxValue
    */
@@ -82,8 +92,6 @@ class PasswordGenerator {
       }
     }
   }
-
-  generateRandom(max: number) {}
 
   /**
    * Empty constructor for the class
