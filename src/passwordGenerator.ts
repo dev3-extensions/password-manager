203b7a6dd69000e57f18ee39f3fd795fad7673ee
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2023-03-18 10:21:42 +0000

    Fix: Merge issues on password generator

--- src/passwordGenerator.ts
+++ src/passwordGenerator.ts
@@ -17,85 +17,6 @@ let passwordLength: number
  * It generates a random password based on the desired length
  */
 class PasswordGenerator {
-<<<<<<< HEAD
-  /**
-   *  Function used to generate random password using Crypto Library
-   * @returns a randomly generated password.
-   * @param length the length value of the password
-   * @param numbers using numbers flag
-   * @param symbols using symbols flag
-   * @returns the password generated
-   */
-  generatePassword(length: number, numbers: boolean, symbols: boolean): string {
-    // Flags to determine if symbols or numbers have been used
-    let isSymbol: boolean = false
-    let isNumber: boolean = false
-    // Initial empty password
-    let password: string = ''
-    let availableChars = PASSWORD_OPTIONS.CHARACTERS_LOWER + PASSWORD_OPTIONS.CHARACTERS_UPPER
-
-    switch (length) {
-      case 0:
-        passwordLength = PASSWORD_LENGTH.SMALL
-        break
-
-      case 1:
-        passwordLength = PASSWORD_LENGTH.MEDIUM
-        break
-
-      case 2:
-        passwordLength = PASSWORD_LENGTH.LONG
-        break
-    }
-
-    // The characters pool
-    const charPool: string[] = []
-
-    if (numbers && symbols) {
-      availableChars += PASSWORD_OPTIONS.NUMBERS + PASSWORD_OPTIONS.SYMBOLS
-    } else if (numbers) {
-      availableChars += PASSWORD_OPTIONS.NUMBERS
-    } else if (symbols) {
-      availableChars += PASSWORD_OPTIONS.SYMBOLS
-    }
-
-    // Adding numbers and symbols
-    while (charPool.length < passwordLength) {
-      // Checking if number has been added
-      if (numbers && !isNumber) {
-        charPool.push(this.genRandomChar(PASSWORD_OPTIONS.NUMBERS))
-        isNumber = true
-      }
-
-      // Checking if symbol as been added
-      if (symbols && !isSymbol) {
-        charPool.push(this.genRandomChar(PASSWORD_OPTIONS.SYMBOLS))
-        isSymbol = true
-      } else {
-        charPool.push(this.genRandomChar(availableChars))
-      }
-    } // End of While
-
-    // Shuffling the list and returning
-    password = this.shuffleList(charPool).join('')
-    return password
-  } // End of Method
-
-  /**
-   * It shuffles characters among random index
-   * @param charArray the Array of string to shuffle
-   * @returns a shuffled array
-   */
-  shuffleList(charArray: string[]) {
-    for (let i = charArray.length - 1; i > 0; i--) {
-      // Generating random index to swap
-      const swapIndex = this.randomNumber(charArray.length)
-      // The character to swap
-      const charToSwap = charArray[i]
-      charArray[i] = charArray[swapIndex]
-      charArray[swapIndex] = charToSwap
-    }
-=======
   /**
    *  Function used to generate random password using Crypto Library
    * @returns a randomly generated password.
@@ -173,7 +94,6 @@ class PasswordGenerator {
       charArray[i] = charArray[swapIndex]
       charArray[swapIndex] = charToSwap
     }
->>>>>>> main
     return charArray
   }
   /**
@@ -191,11 +111,7 @@ class PasswordGenerator {
    * @param maxValue the maximum value randomly generated
    * @returns a random integer between 0 and maxValue
    */
-<<<<<<< HEAD
-  randomNumber(maxValue: number) {
-=======
   randomNumber(maxValue: number): number {
->>>>>>> main
     const noBytes = Math.ceil(Math.log2(maxValue / 8))
     if (!noBytes) {
       return 0
