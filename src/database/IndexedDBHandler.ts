dcced1c0f42e26ddb69a769faba679a6df03316f
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2023-03-29 15:52:14 +0100

    feat: comments added, functions to read data TODO

--- src/database/IndexedDBHandler.ts
+++ src/database/IndexedDBHandler.ts
@@ -12,7 +12,11 @@ if (!indexedDB) {
 
 const request = indexedDB.open(dbName)
 
-function AddEntry(entry: Password) {
+/**
+ * It add an entry into the IndexedDB
+ * @param entry the password to store
+ */
+function addEntry(entry: Password) {
   request.onupgradeneeded = () => {
     db = request.result
     store = db.createObjectStore('Password', {
@@ -22,6 +26,22 @@ function AddEntry(entry: Password) {
 
     store.put({ NAME: entry.name, PASSWORD: entry.password, URL: entry.url })
   }
+}
+
+/**
+ * It fetches all the data from the database
+ */
+function requestAllData() {
+  db = request.result
+  // TODO: Fetching the data from the database
+}
+
+/**
+ * It fetches a specific password based on the index - Name
+ * @param entry The name of the password to fetch interrogating the index
+ */
+function requestOneElement(entry: string) {
+  // TODO: Fetching one password using the index
 }
 
 request.onerror = function (event) {
