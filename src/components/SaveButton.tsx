9be1c280da27ca3173cfa4ee6bf34b06557b5f67
Aryan Prince <aryanprincesanty@icloud.com>
2023-03-31 15:57:39 +0100

    feat: Updated UI design for components

--- src/components/SaveButton.tsx
+++ src/components/SaveButton.tsx
@@ -7,9 +7,10 @@ function SaveButton() {
       type="button"
       aria-label="Save Password"
       className={clsx(
-        'rounded-lg border px-3 py-2 font-medium shadow-lg hover:bg-blue-500/25',
+        'rounded-lg border p-3 font-medium shadow-lg hover:bg-blue-500/25',
         'border-neutral-400/50 bg-neutral-300/50',
-        'dark:border-neutral-600/50 dark:bg-neutral-700/50'
+        'dark:border-neutral-600/50 dark:bg-neutral-700/50',
+        'outline-none focus:ring-offset-2 focus:ring-offset-neutral-800 focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75'
       )}
     >
       <Save size={18} />
