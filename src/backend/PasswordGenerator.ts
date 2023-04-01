bb20c64aa6090fc73cb2f75463baf34c24824f61
Aryan Prince <aryanprincesanty@icloud.com>
2023-04-01 01:27:14 +0100

    fix: Updated imports to fix Vercel deploys again

--- src/backend/PasswordGenerator.ts
+++ src/backend/PasswordGenerator.ts
@@ -1,4 +1,4 @@
-import { PASSWORD_LENGTH } from '../constants/PasswordLength.ts'
+import { PASSWORD_LENGTH } from '../constants/PasswordLength'
 
 /**
  * String of data containing string for all password options
